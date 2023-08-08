744aeab936466a86e36c769a61b032c0f83f15aa
Bob <wasadmin@webagesolutions.com>
2023-08-08 17:02:12 -0400

    initial commit

---
+++ src/main/java/com/teamone/project/AuthServiceSpringBootApp.java
@@ -0,0 +1,13 @@
+package com.teamone.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceSpringBootApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceSpringBootApp.class, args);
+	}
+	
+}
---
+++ src/main/java/com/teamone/project/Token.java
@@ -0,0 +1,22 @@
+package com.teamone.project;
+
+public class Token {
+	
+	String token;
+	
+	public Token () {}
+	
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
