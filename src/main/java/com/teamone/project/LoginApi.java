4745b7b193f60bfe4cb543031cd2150ee804e1b9
Bob <wasadmin@webagesolutions.com>
2023-08-09 15:07:33 -0400

    service

--- src/main/java/com/teamone/project/LoginApi.java
+++ src/main/java/com/teamone/project/LoginApi.java
@@ -1,9 +1,5 @@
 package com.teamone.project;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,11 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.teamone.project.service.AuthServiceImpl;
+import com.teamone.project.service.AuthService;
 
 @RestController
 @CrossOrigin
@@ -25,7 +18,7 @@ import com.teamone.project.service.AuthServiceImpl;
 public class LoginApi {
 
 	@Autowired
-	AuthServiceImpl service;
+	AuthService service;
 	
 	@PostMapping("/token")
 	public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer) {
